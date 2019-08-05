0b369651304e13feba095b36ad06962103c82895
dominik dienlin <dominik.dienlin@gmobis.com>
2019-08-05 19:05:32 +0200

    added script to download resnet50 weights

---
+++ download_weights.sh
@@ -0,0 +1,2 @@
+mkdir -p data/models
+wget -O ./data/models/resnet50_weights_tf_dim_ordering_tf_kernels.h5 https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels.h5 --no-check-certificate
